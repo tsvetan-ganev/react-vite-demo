335aa6beae6078dc82947c349000b4bd1b7151fa
Tsvetan Ganev <tsetso.ganev@gmail.com>
2024-11-26 00:46:12 +0200

    Add github actions

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import react from 'eslint-plugin-react';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'coverage', 'public'] },
   {
     settings: { react: { version: '18.3' } },
     extends: [
